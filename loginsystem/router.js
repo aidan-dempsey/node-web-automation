950e10a8c4cb10b85c5bac0a07e838cc3be21b9c
ApiPistek <apollinep10@gmail.com>
2021-05-24 16:45:40 -0600

    auto redirect

--- loginsystem/router.js
+++ loginsystem/router.js
@@ -9,11 +9,21 @@ const credential = {
 router.post('/login', (req,res) => {
     if(req.body.username==credential.username && req.body.password==credential.password){
         req.session.user = req.body.username;
-        res.redirect('/auto');
+        res.redirect('/route/auto');
     }else{
         res.end('Invalid username or password')
     }
 
 });
+
+
+router.get('/auto', (req,res) => {
+    if(req.session.user){
+        res.render('auto')
+    }else{
+    res.send('Unauthorized user')}
+});
+
+
 
 module.exports = router;
